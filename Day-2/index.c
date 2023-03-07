0bceacae3bbb491bc0f2fbfeb55c4e755d733f34
mahedi <mahedianik4@gmail.com>
2023-03-07 13:18:09 +0600

    learn data type char, int , float dubble data type,

--- Day-2/index.c
+++ Day-2/index.c
@@ -1,7 +1,35 @@
+// #include<stdio.h>
+// int main(){
+
+//     printf("hi\nmehedi");
+
+//     return 0;
+// }
+
+// #include<stdio.h>
+// int main(){
+// int x= 1000; ;
+
+//     printf("my salary %d taka",x+1);
+
+//     return 0;
+// }
+// #include<stdio.h>
+// int main(){
+// char x= 'a'; 
+
+//     printf("%c",x);
+
+//     return 0;
+// }
 #include<stdio.h>
 int main(){
-
-    printf("hi\nmehedi");
+int x,y,n; 
+x=10;
+y=10;
+n = x+y;
+    printf("%d",n);
 
     return 0;
 }
+
